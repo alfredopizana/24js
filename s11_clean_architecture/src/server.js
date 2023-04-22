12962cfde391a3b1dc0fdf243541d8c1eae05cd1
alfredopizana <pizana.alfredo@gmail.com>
2023-04-22 00:13:50 -0600

    Added cors

--- s11_clean_architecture/src/server.js
+++ s11_clean_architecture/src/server.js
@@ -6,11 +6,13 @@
  */
 
 import express from 'express'
+import cors from 'cors'
 import koderRouter from './routers/koder.router.js'
 import authRouter from './routers/auth.router.js'
 const server = express() // Crear el server
 
 // Middlewares
+server.use(cors()) // Va a permitir que su frontend haga peticiones al backend
 server.use(express.json())
 
 // Routers
